54f5371edc44b309d35c160065df62d42c2db84d
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-03-13 15:23:21 +0530

    Practise

---
+++ src/Practice/Pract.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Pract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your name : ");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is :" + name);
+    }
+}
