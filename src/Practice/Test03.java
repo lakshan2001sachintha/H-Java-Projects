e3bdcce61db0f9d33a732f3525519ffeaed77d1d
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-03-15 23:25:23 +0530

    Test 03

---
+++ src/Practice/Test03.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class Test03 {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+
+        cars.add("A");
+        System.out.println();
+    }
+}
